0908191a66897773bdb0535970bb3af2f373ee7c
Margo Potylitsyna <148562840+Ritumka@users.noreply.github.com>
2025-07-29 16:58:48 +0000

    add task for render

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :teams
 
   root "players#index"
-  
+
+  get("/rake_tasks", { :controller => "rake_tasks", :action => "show" })
+  get("/run_task", { :controller => "rake_tasks", :action => "run_task" })
 end
